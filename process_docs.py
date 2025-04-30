b7820d3355489c4a69ddbb945a55c60e36140fc8
Wesley Almeida <wesleyinfo3@gmail.com>
2025-04-30 15:53:24 -0300

    Update process_docs.py

--- process_docs.py
+++ process_docs.py
@@ -7,7 +7,7 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 import google.generativeai as genai
 import logging
-
+ 
 # Configuração de logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
